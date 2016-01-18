e1ecf6cde6d2e9e90050fb7d7df2a30fea51237e
Jeanneret Hugo (CPLN - ET) <JeanneretH2@s2.rpn.ch>
2016-01-18 16:26:26 +0100

    Ajout des classes hero et pouvoir heroique.

---
+++ Pierre-de-Foyer/Pierre-de-Foyer/Classes/Hero.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pierre_de_Foyer.Classes
+{
+    class Hero
+    {
+        private string strNom;
+        private int iPointVie;
+        private int iAttaque;
+        private PouvoirHeroique _pouvoirHeroique;
+        private List<Carte> _cartes = new List<Carte>();
+
+        public void Attaquer()
+        {
+
+        }
+
+        public void PrendreDegats()
+        {
+
+        }
+
+        public int getPointDeVie()
+        {
+            return iPointVie;
+        }
+
+        public int getAttaque()
+        {
+            return iAttaque;
+        }
+
+        public void PiocherCartes()
+        {
+
+        }
+
+        public PouvoirHeroique getPouvoirHeroique()
+        {
+            return _pouvoirHeroique;
+        }
+    }
+}
---
+++ Pierre-de-Foyer/Pierre-de-Foyer/Classes/PouvoirHeroique.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pierre_de_Foyer.Classes
+{
+    class PouvoirHeroique
+    {
+        private int iCoutMana;
+        private string strNomPouvoir;
+
+        public void ActionPouvoirHeroique()
+        {
+
+        }
+    }
+}
