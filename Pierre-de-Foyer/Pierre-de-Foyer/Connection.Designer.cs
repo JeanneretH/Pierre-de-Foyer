1b1e2b43d7ebadfd8917f9f51d4e45720d092513
Loison Yohann <LoisonYo@s2.rpn.ch>
2016-01-18 14:35:54 +0100

    Ajout de l'image d'intro et commencement du menu de connection

--- Pierre-de-Foyer/Pierre-de-Foyer/Connection.Designer.cs
+++ Pierre-de-Foyer/Pierre-de-Foyer/Connection.Designer.cs
@@ -28,12 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnConnection = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.tbxUsername = new System.Windows.Forms.TextBox();
+            this.tbxPass = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConnection
+            // 
+            this.btnConnection.Location = new System.Drawing.Point(128, 173);
+            this.btnConnection.Name = "btnConnection";
+            this.btnConnection.Size = new System.Drawing.Size(75, 23);
+            this.btnConnection.TabIndex = 0;
+            this.btnConnection.Text = "Connection";
+            this.btnConnection.UseVisualStyleBackColor = true;
+            this.btnConnection.Click += new System.EventHandler(this.btnConnection_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(128, 220);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(75, 23);
+            this.btnQuit.TabIndex = 1;
+            this.btnQuit.Text = "Quitter";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(116, 68);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(88, 13);
+            this.lblUsername.TabIndex = 2;
+            this.lblUsername.Text = "Nom de compte :";
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Location = new System.Drawing.Point(116, 113);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(71, 13);
+            this.lblPass.TabIndex = 3;
+            this.lblPass.Text = "Mot de passe";
+            // 
+            // tbxUsername
+            // 
+            this.tbxUsername.Location = new System.Drawing.Point(119, 84);
+            this.tbxUsername.Name = "tbxUsername";
+            this.tbxUsername.Size = new System.Drawing.Size(100, 20);
+            this.tbxUsername.TabIndex = 4;
+            // 
+            // tbxPass
+            // 
+            this.tbxPass.Location = new System.Drawing.Point(119, 129);
+            this.tbxPass.Name = "tbxPass";
+            this.tbxPass.Size = new System.Drawing.Size(100, 20);
+            this.tbxPass.TabIndex = 5;
+            // 
+            // Connection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(390, 375);
+            this.Controls.Add(this.tbxPass);
+            this.Controls.Add(this.tbxUsername);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnConnection);
+            this.Name = "Connection";
+            this.Text = "Pierre-de-Foyer";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Connection_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnConnection;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.TextBox tbxUsername;
+        private System.Windows.Forms.TextBox tbxPass;
     }
 }
 
--- Pierre-de-Foyer/Pierre-de-Foyer/Connection.cs
+++ Pierre-de-Foyer/Pierre-de-Foyer/Connection.cs
@@ -16,5 +16,46 @@ namespace Pierre_de_Foyer
         {
             InitializeComponent();
         }
+
+        private void Connection_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Permet a l'utilisateur de rentrer dans le menu si le test d'identifiant est correcte.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConnection_Click(object sender, EventArgs e)
+        {
+            if(VerifieIdentifiant() == true)
+            {
+                Form menu = new Menu();
+            }
+            else
+            {
+                MessageBox.Show("Le nom de compte ou le mot de passe sont incorrect", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        /// <summary>
+        /// Vérifie si les identifiants rentré sont inclu dans les compte.
+        /// </summary>
+        /// <returns>Retourn true si les identifiants sont correcte</returns>
+        private bool VerifieIdentifiant()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Permet à l'utilisateur de quitter le programme
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
