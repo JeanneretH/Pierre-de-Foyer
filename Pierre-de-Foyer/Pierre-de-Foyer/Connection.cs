f9ced9ac151c200dca4d73fd15a4737697efe9ca
Loison Yohann <LoisonYo@s2.rpn.ch>
2016-01-18 14:43:24 +0100

    Finission de la fonction btnConnection_Clic()

--- Pierre-de-Foyer/Pierre-de-Foyer/Connection.cs
+++ Pierre-de-Foyer/Pierre-de-Foyer/Connection.cs
@@ -31,7 +31,11 @@ namespace Pierre_de_Foyer
         {
             if(VerifieIdentifiant() == true)
             {
+                Form connection = new Connection();
                 Form menu = new Menu();
+                menu.Show();
+                connection.Visible = false;
+                
             }
             else
             {
