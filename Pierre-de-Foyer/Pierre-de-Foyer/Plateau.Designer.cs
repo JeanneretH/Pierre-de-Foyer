f8bea8c9098cac14477d93d4e50b26ced144417c
LoisonYo <yohann.loison@rpn.ch>
2016-01-21 10:34:48 +0100

    Hero et Pouvoir heroique sur le plateau
    Ajout des hero et des pouvoir heroique au plateau de jeu

--- Pierre-de-Foyer/Pierre-de-Foyer/Plateau.Designer.cs
+++ Pierre-de-Foyer/Pierre-de-Foyer/Plateau.Designer.cs
@@ -29,22 +29,76 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Plateau));
+            this.pbxHero = new System.Windows.Forms.PictureBox();
+            this.pbxHeroAdverse = new System.Windows.Forms.PictureBox();
+            this.pbxPouvoirHero = new System.Windows.Forms.PictureBox();
+            this.pbxPouvoirHeroAdverse = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxHero)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxHeroAdverse)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPouvoirHero)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPouvoirHeroAdverse)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pbxHero
+            // 
+            this.pbxHero.Location = new System.Drawing.Point(12, 12);
+            this.pbxHero.Name = "pbxHero";
+            this.pbxHero.Size = new System.Drawing.Size(150, 150);
+            this.pbxHero.TabIndex = 0;
+            this.pbxHero.TabStop = false;
+            // 
+            // pbxHeroAdverse
+            // 
+            this.pbxHeroAdverse.Location = new System.Drawing.Point(12, 168);
+            this.pbxHeroAdverse.Name = "pbxHeroAdverse";
+            this.pbxHeroAdverse.Size = new System.Drawing.Size(150, 150);
+            this.pbxHeroAdverse.TabIndex = 1;
+            this.pbxHeroAdverse.TabStop = false;
+            // 
+            // pbxPouvoirHero
+            // 
+            this.pbxPouvoirHero.Location = new System.Drawing.Point(168, 12);
+            this.pbxPouvoirHero.Name = "pbxPouvoirHero";
+            this.pbxPouvoirHero.Size = new System.Drawing.Size(50, 50);
+            this.pbxPouvoirHero.TabIndex = 2;
+            this.pbxPouvoirHero.TabStop = false;
+            // 
+            // pbxPouvoirHeroAdverse
+            // 
+            this.pbxPouvoirHeroAdverse.Location = new System.Drawing.Point(168, 68);
+            this.pbxPouvoirHeroAdverse.Name = "pbxPouvoirHeroAdverse";
+            this.pbxPouvoirHeroAdverse.Size = new System.Drawing.Size(50, 50);
+            this.pbxPouvoirHeroAdverse.TabIndex = 3;
+            this.pbxPouvoirHeroAdverse.TabStop = false;
             // 
             // Plateau
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(1353, 718);
+            this.Controls.Add(this.pbxPouvoirHeroAdverse);
+            this.Controls.Add(this.pbxPouvoirHero);
+            this.Controls.Add(this.pbxHeroAdverse);
+            this.Controls.Add(this.pbxHero);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Plateau";
             this.Text = "Plateau";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Plateau_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxHero)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxHeroAdverse)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPouvoirHero)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPouvoirHeroAdverse)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbxHero;
+        private System.Windows.Forms.PictureBox pbxHeroAdverse;
+        private System.Windows.Forms.PictureBox pbxPouvoirHero;
+        private System.Windows.Forms.PictureBox pbxPouvoirHeroAdverse;
     }
 }
--- Pierre-de-Foyer/Pierre-de-Foyer/Plateau.cs
+++ Pierre-de-Foyer/Pierre-de-Foyer/Plateau.cs
@@ -16,5 +16,24 @@ namespace Pierre_de_Foyer
         {
             InitializeComponent();
         }
+
+        private void Plateau_Load(object sender, EventArgs e)
+        {
+            //Initialisation
+            Classes.Hero hero = new Classes.Hero();
+            Classes.Hero heroAdverce = new Classes.Hero();
+
+            //Image des objet (Temporaire)
+            pbxHero.BackColor = Color.Green;
+            pbxHeroAdverse.BackColor = Color.Red;
+            pbxPouvoirHero.BackColor = Color.Yellow;
+            pbxPouvoirHeroAdverse.BackColor = Color.Yellow;
+
+            //Placement des objet
+            pbxHero.Location = new Point(this.Width / 2 - pbxHero.Width / 2, this.Height - pbxHeroAdverse.Height);
+            pbxHeroAdverse.Location = new Point(this.Width / 2 - pbxHeroAdverse.Width / 2, 0);
+            pbxPouvoirHero.Location = new Point(this.Width / 2 + pbxHero.Width / 2, this.Height - pbxPouvoirHero.Height);
+            pbxPouvoirHeroAdverse.Location = new Point(this.Width / 2 - pbxHeroAdverse.Width / 2 - pbxPouvoirHeroAdverse.Width, 0);
+        }
     }
 }
