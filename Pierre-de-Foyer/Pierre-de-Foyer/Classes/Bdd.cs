f28471ec60b2074efd88f14d7ddc25a57a1642f8
MoulinVi <vincent.moulin@rpn.ch>
2016-01-25 20:32:38 +0100

    Modification des menus (très bof, compliqué de pas ralentir le programme)

--- Pierre-de-Foyer/Pierre-de-Foyer/Classes/Bdd.cs
+++ Pierre-de-Foyer/Pierre-de-Foyer/Classes/Bdd.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
+//using MySql.Data.MySqlClient;
 
 namespace Pierre_de_Foyer.Classes
 {
--- Pierre-de-Foyer/Pierre-de-Foyer/Inscription.Designer.cs
+++ Pierre-de-Foyer/Pierre-de-Foyer/Inscription.Designer.cs
@@ -39,19 +39,21 @@
             // 
             // btnInscription
             // 
-            this.btnInscription.Location = new System.Drawing.Point(12, 94);
+            this.btnInscription.Font = new System.Drawing.Font("Candara", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.btnInscription.Location = new System.Drawing.Point(201, 214);
             this.btnInscription.Name = "btnInscription";
-            this.btnInscription.Size = new System.Drawing.Size(201, 29);
+            this.btnInscription.Size = new System.Drawing.Size(256, 53);
             this.btnInscription.TabIndex = 1;
-            this.btnInscription.Text = "Inscription";
+            this.btnInscription.Text = "Inscrivez-vous !";
             this.btnInscription.UseVisualStyleBackColor = true;
             this.btnInscription.Click += new System.EventHandler(this.NewCompte_Click);
             // 
             // btnRetourMenu
             // 
-            this.btnRetourMenu.Location = new System.Drawing.Point(12, 129);
+            this.btnRetourMenu.Font = new System.Drawing.Font("Candara", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.btnRetourMenu.Location = new System.Drawing.Point(201, 273);
             this.btnRetourMenu.Name = "btnRetourMenu";
-            this.btnRetourMenu.Size = new System.Drawing.Size(201, 29);
+            this.btnRetourMenu.Size = new System.Drawing.Size(256, 53);
             this.btnRetourMenu.TabIndex = 1;
             this.btnRetourMenu.Text = "Retour au menu";
             this.btnRetourMenu.UseVisualStyleBackColor = true;
@@ -60,40 +62,46 @@
             // lblNom
             // 
             this.lblNom.AutoSize = true;
-            this.lblNom.Location = new System.Drawing.Point(13, 13);
+            this.lblNom.Font = new System.Drawing.Font("Candara", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.lblNom.ForeColor = System.Drawing.Color.White;
+            this.lblNom.Location = new System.Drawing.Point(202, 101);
             this.lblNom.Name = "lblNom";
-            this.lblNom.Size = new System.Drawing.Size(55, 13);
+            this.lblNom.Size = new System.Drawing.Size(120, 29);
             this.lblNom.TabIndex = 2;
             this.lblNom.Text = "Votre nom";
             // 
             // tbxNom
             // 
-            this.tbxNom.Location = new System.Drawing.Point(12, 29);
+            this.tbxNom.Location = new System.Drawing.Point(201, 133);
             this.tbxNom.Name = "tbxNom";
-            this.tbxNom.Size = new System.Drawing.Size(201, 20);
+            this.tbxNom.Size = new System.Drawing.Size(256, 20);
             this.tbxNom.TabIndex = 5;
             // 
             // lblMotDePasse
             // 
             this.lblMotDePasse.AutoSize = true;
-            this.lblMotDePasse.Location = new System.Drawing.Point(13, 52);
+            this.lblMotDePasse.Font = new System.Drawing.Font("Candara", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.lblMotDePasse.ForeColor = System.Drawing.Color.White;
+            this.lblMotDePasse.Location = new System.Drawing.Point(202, 156);
             this.lblMotDePasse.Name = "lblMotDePasse";
-            this.lblMotDePasse.Size = new System.Drawing.Size(98, 13);
+            this.lblMotDePasse.Size = new System.Drawing.Size(208, 29);
             this.lblMotDePasse.TabIndex = 2;
             this.lblMotDePasse.Text = "Votre mot de passe";
             // 
             // tbxMotDePasse
             // 
-            this.tbxMotDePasse.Location = new System.Drawing.Point(12, 68);
+            this.tbxMotDePasse.Location = new System.Drawing.Point(201, 188);
             this.tbxMotDePasse.Name = "tbxMotDePasse";
-            this.tbxMotDePasse.Size = new System.Drawing.Size(201, 20);
+            this.tbxMotDePasse.Size = new System.Drawing.Size(256, 20);
             this.tbxMotDePasse.TabIndex = 5;
+            this.tbxMotDePasse.TextChanged += new System.EventHandler(this.tbxMotDePasse_TextChanged);
             // 
             // Inscription
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(661, 544);
             this.Controls.Add(this.tbxMotDePasse);
             this.Controls.Add(this.tbxNom);
             this.Controls.Add(this.lblMotDePasse);
