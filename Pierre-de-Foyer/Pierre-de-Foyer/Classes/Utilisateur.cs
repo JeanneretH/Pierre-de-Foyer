bc048e7bc1bfd4b99cf3ed3420b5a572af37a89d
LoisonYo <yohann.loison@rpn.ch>
2016-01-26 20:18:35 +0100

    Création de la classe Utilisateur

---
+++ Pierre-de-Foyer/Pierre-de-Foyer/Classes/Utilisateur.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pierre_de_Foyer.Classes
+{
+    class Utilisateur
+    {
+        //Objets
+        private String strNom = "";
+        private Hero HeroUtilisateur;
+
+        //Constructeurs
+        public Utilisateur(string nom)
+        {
+            this.strNom = nom;
+        }
+
+        public Hero getHeroFromUser()
+        {
+            List<Carte> DefaultDeck = new List<Carte>();
+            switch(strNom)
+            {
+                case "Yohann":
+                    {
+                        for (int i = 0; i < 30; i++)
+                        {
+                            Serviteur Card = new Serviteur(1, 1, "Salut c'est lama", "Je savais pas quoi mettre", "Lamacorne", Properties.Resources.Lamacorne_Temporaire, 1, 1, true, true);
+                            DefaultDeck.Add(Card);
+                        }
+                        HeroUtilisateur = new Hero("Yohann", 30, 0, null, DefaultDeck);
+                    }
+                    break;
+                case "Sacha":
+                    {
+                        for (int i = 0; i < 30; i++)
+                        {
+                            Serviteur Card = new Serviteur(1, 1, "Salut c'est lama", "Je savais pas quoi mettre", "Lamacorne", Properties.Resources.Lamacorne_Temporaire, 1, 1, true, true);
+                            DefaultDeck.Add(Card);
+                        }
+                        HeroUtilisateur = new Hero("Sacha", 30, 0, null, DefaultDeck);
+                    }
+                    break;
+                case "Vincent":
+                    {
+                        for (int i = 0; i < 30; i++)
+                        {
+                            Serviteur Card = new Serviteur(1, 1, "Salut c'est lama", "Je savais pas quoi mettre", "Lamacorne", Properties.Resources.Lamacorne_Temporaire, 1, 1, true, true);
+                            DefaultDeck.Add(Card);
+                        }
+                        HeroUtilisateur = new Hero("Vincent", 30, 0, null, DefaultDeck);
+                    }
+                    break;
+                case "Hugo":
+                    {
+                        for (int i = 0; i < 30; i++)
+                        {
+                            Serviteur Card = new Serviteur(1, 1, "Salut c'est lama", "Je savais pas quoi mettre", "Lamacorne", Properties.Resources.Lamacorne_Temporaire, 1, 1, true, true);
+                            DefaultDeck.Add(Card);
+                        }
+                        HeroUtilisateur = new Hero("Hugo", 30, 0, null, DefaultDeck);
+                    }
+                    break;
+                case "Janis":
+                    {
+                        for (int i = 0; i < 30; i++)
+                        {
+                            Serviteur Card = new Serviteur(1, 1, "Salut c'est lama", "Je savais pas quoi mettre", "Lamacorne", Properties.Resources.Lamacorne_Temporaire, 1, 1, true, true);
+                            DefaultDeck.Add(Card);
+                        }
+                        HeroUtilisateur = new Hero("Janis", 30, 0, null, DefaultDeck);
+                    }
+                    break;
+                case "Romain":
+                    {
+                        for (int i = 0; i < 30; i++)
+                        {
+                            Serviteur Card = new Serviteur(1, 1, "Salut c'est lama", "Je savais pas quoi mettre", "Lamacorne", Properties.Resources.Lamacorne_Temporaire, 1, 1, true, true);
+                            DefaultDeck.Add(Card);
+                        }
+                        HeroUtilisateur = new Hero("Romain", 30, 0, null, DefaultDeck);
+                    }
+                    break;
+                default:
+                    {
+                        for (int i = 0; i < 30; i++)
+                        {
+                            Serviteur Card = new Serviteur(1, 1, "Salut c'est lama", "Je savais pas quoi mettre", "Lamacorne", Properties.Resources.Lamacorne_Temporaire, 1, 1, true, true);
+                            DefaultDeck.Add(Card);
+                        }
+                        HeroUtilisateur = new Hero("Default", 30, 0, null, DefaultDeck);
+                    }
+                    break;
+            }
+            return HeroUtilisateur;
+        }
+    }
+}
--- Pierre-de-Foyer/Pierre-de-Foyer/Properties/Resources.Designer.cs
+++ Pierre-de-Foyer/Pierre-de-Foyer/Properties/Resources.Designer.cs
@@ -109,5 +109,15 @@ namespace Pierre_de_Foyer.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Lamacorne_Temporaire {
+            get {
+                object obj = ResourceManager.GetObject("Lamacorne_Temporaire", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
